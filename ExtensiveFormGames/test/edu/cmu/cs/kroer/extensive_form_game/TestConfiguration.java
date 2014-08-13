e5051cee9a56a870264c9a866fccf7705cf6a90b
ChrKroer <chrkroer@gmail.com>
2014-08-12 20:14:14 -0400

    added comments to describe the use of the config string

--- ExtensiveFormGames/test/edu/cmu/cs/kroer/extensive_form_game/TestConfiguration.java
+++ ExtensiveFormGames/test/edu/cmu/cs/kroer/extensive_form_game/TestConfiguration.java
@@ -1,7 +1,10 @@
 package edu.cmu.cs.kroer.extensive_form_game;
 
 public final class TestConfiguration {
+	// Specifies where game instance files in the new format are located
 	static final String gamesFolder = "/Users/ckroer/Documents/research/zerosum/games/";
+	// Specifies where game instance files in the zerosum package format are located
 	static final String zerosumGamesFolder = "/Users/ckroer/Documents/research/zerosum/original_games/";
+	// Specifies where to save LP models. Each test case dumps its LP model to a file in the specified folder
 	static final String lpModelsFolder = "/Users/ckroer/Documents/research/lp-models/";
 }
